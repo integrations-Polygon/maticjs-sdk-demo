8c2ef27d6cd7ec4b99e2f0a84f2ff97409aeb62c
Shivam Kalra <shivamkalra98@gmail.com>
2022-08-10 18:33:03 +0530

    feat: child/parent provider

---
+++ src/provider.ts
@@ -0,0 +1,36 @@
+import { getDefaultProvider, providers } from "ethers";
+import config from "../config";
+
+const { chainId } = config;
+
+const childProviderList = [
+  // public providers
+  new providers.AlchemyProvider("maticmum"),
+  new providers.InfuraProvider("maticmum"),
+  new providers.JsonRpcProvider("https://matic-mumbai.chainstacklabs.com"),
+  // new providers.JsonRpcProvider("https://rpc.ankr.com/polygon_mumbai"),
+];
+
+const parentProviderList = [
+  new providers.AlchemyProvider("goerli"),
+  new providers.InfuraProvider("goerli"),
+  // new providers.JsonRpcProvider("https://rpc.goerli.mudit.blog"),
+
+  new providers.JsonRpcProvider(
+    "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+  ),
+];
+
+const parentProvider =
+  chainId.root === "1"
+    ? getDefaultProvider(parseInt(chainId.root))
+    : new providers.FallbackProvider(parentProviderList);
+const childProvider =
+  chainId.child === "137"
+    ? getDefaultProvider(parseInt(chainId.child))
+    : new providers.FallbackProvider(childProviderList);
+
+export default {
+  parent: parentProvider,
+  child: childProvider,
+};
